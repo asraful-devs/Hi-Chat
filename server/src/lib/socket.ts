fe0607729c1756da8499fd51a396e52df721e0b5
Md Asraful <asraful.devs@gmail.com>
2026-02-22 15:24:33 +0600

    socket io server connect

---
+++ server/src/lib/socket.ts
@@ -0,0 +1,53 @@
+import express from 'express';
+import http from 'http';
+import { Server, Socket } from 'socket.io';
+import { socketAuthMiddleware } from '../middleware/socket.auth.middleware.ts';
+import type { IUser } from '../types/user.type.ts';
+import { ENV } from './env.ts';
+
+interface CustomSocket extends Socket {
+    user?: IUser;
+    userId?: string;
+}
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: [ENV.CLIENT_URL],
+        credentials: true,
+    },
+});
+
+// apply authentication middleware to all socket connections
+io.use(socketAuthMiddleware);
+
+// we will use this function to check if the user is online or not
+export function getReceiverSocketId(userId: string) {
+    return userSocketMap[userId];
+}
+
+// this is for storig online users
+const userSocketMap: { [key: string]: string } = {}; // {userId:socketId}
+
+io.on('connection', (socket: CustomSocket) => {
+    console.log('A user connected', socket.user?.fullName);
+
+    const userId = socket.userId;
+    if (!userId) return;
+
+    userSocketMap[userId] = socket.id;
+
+    // io.emit() is used to send events to all connected clients
+    io.emit('getOnlineUsers', Object.keys(userSocketMap));
+
+    // with socket.on we listen for events from clients
+    socket.on('disconnect', () => {
+        console.log('A user disconnected', socket.user?.fullName);
+        delete userSocketMap[userId];
+        io.emit('getOnlineUsers', Object.keys(userSocketMap));
+    });
+});
+
+export { app, io, server };
